49b7b12b51d2e4c8495a6b28f279733f5634a98e
Ionut Alixandroae <IAlixandroae@euspaceimaging.local>
2019-03-22 16:24:58 +0100

    Initial commit

---
+++ main.js
@@ -0,0 +1,16 @@
+$(document).ready( function () {
+    $('.table').DataTable();
+    
+    // $('.dataTables_paginate a.paginate_button.previous').addClass('btn btn-transparent');
+    // $('.dataTables_paginate a.paginate_button.next').addClass('btn btn-transparent btn-arrow');
+    // $('.dataTables_paginate a.paginate_button').addClass('btn btn-transparent');
+    // $('.dataTables_paginate a.paginate_button.current').removeClass('btn-transparent');
+    
+    // $('.table').on( 'page.dt', function () {
+    //     console.log($('.dataTables_paginate a.paginate_button.current').attr('class'))
+    //     $('.dataTables_paginate a.paginate_button.previous').addClass('btn btn-transparent');
+    //     $('.dataTables_paginate a.paginate_button.next').addClass('btn btn-transparent btn-arrow');
+    //     $('.dataTables_paginate a.paginate_button').addClass('btn btn-transparent');
+    //     $('.dataTables_paginate a.paginate_button.current').removeClass('btn-transparent');
+    // } );
+});
